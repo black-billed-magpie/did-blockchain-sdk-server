d6fad8aaccb56b7869dcea650e5358842edaefbe
mykim <mykim@raoncorp.com>
2025-04-17 15:07:14 +0900

    feat: Enhance EvmContractApi to decode and construct DidDocument from blockchain response

--- source/did-blockchain-sdk-server/src/main/java/org/omnione/did/ethereum/EvmContractApi.java
+++ source/did-blockchain-sdk-server/src/main/java/org/omnione/did/ethereum/EvmContractApi.java
@@ -3,6 +3,7 @@ package org.omnione.did.ethereum;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
@@ -24,11 +25,15 @@ import org.omnione.sender.ethereum.ContractFunctionName;
 import org.omnione.sender.ethereum.EvmContractData;
 import org.omnione.sender.ethereum.EvmSender;
 import org.omnione.sender.ethereum.EvmServerInformation;
+import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
+import org.web3j.abi.Utils;
 import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.DynamicArray;
+import org.web3j.abi.datatypes.DynamicStruct;
 import org.web3j.abi.datatypes.Type;
 import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.Uint8;
 
 public class EvmContractApi implements ContractApi {
 
@@ -130,7 +135,130 @@ public class EvmContractApi implements ContractApi {
         outputParams
     );
 
-    return send(contractData);
+    byte[] documentBytes = send(contractData);
+    if (documentBytes == null) {
+      throw new BlockChainException(
+          BlockchainErrorCode.TRANSACTION_ERROR, new Error("Transaction failed"));
+
+    }
+    List<Type> decodedData = FunctionReturnDecoder.decode(
+        new String(documentBytes), Utils.convert(outputParams));
+
+    List<Type> decodedDynamicStruct = ((DynamicStruct) decodedData.get(0)).getValue();
+
+    DynamicArray<Utf8String> context = (DynamicArray<Utf8String>) decodedDynamicStruct.get(0);
+    Utf8String id = (Utf8String) decodedDynamicStruct.get(1);
+    Utf8String controller = (Utf8String) decodedDynamicStruct.get(2);
+    Utf8String created = (Utf8String) decodedDynamicStruct.get(3);
+    Utf8String updated = (Utf8String) decodedDynamicStruct.get(4);
+    Utf8String versionId = (Utf8String) decodedDynamicStruct.get(5);
+    Bool deactivated = (Bool) decodedDynamicStruct.get(6);
+    DynamicArray<DynamicStruct> verificationMethodStruct = (DynamicArray<DynamicStruct>) decodedDynamicStruct.get(
+        7);
+    DynamicArray<Utf8String> assertionMethod = (DynamicArray<Utf8String>) decodedDynamicStruct.get(
+        8);
+    DynamicArray<Utf8String> authentication = (DynamicArray<Utf8String>) decodedDynamicStruct.get(
+        9);
+    DynamicArray<Utf8String> keyAgreement = (DynamicArray<Utf8String>) decodedDynamicStruct.get(10);
+    DynamicArray<Utf8String> capabilityInvocation = (DynamicArray<Utf8String>) decodedDynamicStruct.get(
+        11);
+    DynamicArray<Utf8String> capabilityDelegation = (DynamicArray<Utf8String>) decodedDynamicStruct.get(
+        12);
+    DynamicArray<DynamicStruct> services = (DynamicArray<DynamicStruct>) decodedDynamicStruct.get(
+        13);
+
+    DidDocument didDocument = new DidDocument();
+    didDocument.setContext(context.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+    didDocument.setId(id.getValue());
+    didDocument.setController(controller.getValue());
+    didDocument.setCreated(created.getValue());
+    didDocument.setUpdated(updated.getValue());
+    didDocument.setVersionId(versionId.getValue());
+    didDocument.setDeactivated(deactivated.getValue());
+
+    List<org.omnione.did.data.model.did.VerificationMethod> verificationMethodList = new ArrayList<>();
+    for (int i = 0; i < verificationMethodStruct.getValue()
+        .size(); i++) {
+      DynamicStruct verificationMethodStructItem = verificationMethodStruct.getValue()
+          .get(i);
+      Utf8String idValue = (Utf8String) verificationMethodStructItem.getValue()
+          .get(0);
+      LOG.info(() -> "idValue: " + idValue.getValue());
+      Uint8 typeValue = (Uint8) verificationMethodStructItem.getValue()
+          .get(1);
+      Utf8String controllerValue = (Utf8String) verificationMethodStructItem.getValue()
+          .get(2);
+      Utf8String publicKeyMultibaseValue = (Utf8String) verificationMethodStructItem.getValue()
+          .get(3);
+      Uint8 authTypeValue = (Uint8) verificationMethodStructItem.getValue()
+          .get(4);
+
+      org.omnione.did.data.model.did.VerificationMethod verificationMethod = new org.omnione.did.data.model.did.VerificationMethod();
+      verificationMethod.setId(idValue.getValue());
+      verificationMethod.setType(String.valueOf(typeValue.getValue()));
+      verificationMethod.setController(controllerValue.getValue());
+      verificationMethod.setPublicKeyMultibase(publicKeyMultibaseValue.getValue());
+      verificationMethod.setAuthType(authTypeValue.getValue()
+          .intValue());
+
+      verificationMethodList.add(verificationMethod);
+    }
+
+    didDocument.setVerificationMethod(verificationMethodList);
+    didDocument.setAssertionMethod(assertionMethod.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+    didDocument.setAuthentication(authentication.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+    didDocument.setKeyAgreement(keyAgreement.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+    didDocument.setCapabilityInvocation(capabilityInvocation.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+    didDocument.setCapabilityDelegation(capabilityDelegation.getValue()
+        .stream()
+        .map(Utf8String::getValue)
+        .toList());
+
+    List<org.omnione.did.data.model.did.Service> servicesList = new ArrayList<>();
+    for (int i = 0; i < services.getValue()
+        .size(); i++) {
+      DynamicStruct serviceStructItem = services.getValue()
+          .get(i);
+      Utf8String idValue = (Utf8String) serviceStructItem.getValue()
+          .get(0);
+      Utf8String typeValue = (Utf8String) serviceStructItem.getValue()
+          .get(1);
+      DynamicArray<Utf8String> serviceEndpointValue = (DynamicArray<Utf8String>) serviceStructItem.getValue()
+          .get(2);
+
+      List<String> serviceEndpointList = new ArrayList<>();
+      for (int j = 0; j < serviceEndpointValue.getValue()
+          .size(); j++) {
+        Utf8String endpointItem = serviceEndpointValue.getValue()
+            .get(j);
+        serviceEndpointList.add(endpointItem.getValue());
+      }
+
+      org.omnione.did.data.model.did.Service service = new org.omnione.did.data.model.did.Service();
+      service.setId(idValue.getValue());
+      service.setType(typeValue.getValue());
+      service.setServiceEndpoint(serviceEndpointList);
+
+      servicesList.add(service);
+    }
+    didDocument.setService(servicesList);
+
+    return didDocument;
   }
 
   @Override
